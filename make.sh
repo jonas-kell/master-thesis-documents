1a1cc86dbd48c3131ca5a38430234a8008be6f69
Jonas Kell <jonas-kell@web.de>
2024-12-08 17:04:45 +0100

    fix: compile all gnuplot scripts

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 echo "Generating Gnuplot scripts"
 
 plotfolders=( "binary-entropy-function" "concurrence-from-spin" "energy-variance" "j-sweep" "monte-carlo-variance-test" "vcn-eff-stepsize"  )
-plotfolders=( "vcn-eff-stepsize"  )
+
 for plotfolder in "${plotfolders[@]}"; do
     "./thesis-latex/plotgeneration/$plotfolder/make.sh"
 done
