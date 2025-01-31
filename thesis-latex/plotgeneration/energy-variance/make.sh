2f986041dd5b538cd835639ba45b424c14ce5d64
Jonas Kell <jonas-kell@web.de>
2025-01-31 13:15:29 +0100

    refactor: split energy variance experiment to own runners (hpc does not manage to diagonalize more than n=4 chain exactly)

--- thesis-latex/plotgeneration/energy-variance/make.sh
+++ thesis-latex/plotgeneration/energy-variance/make.sh
@@ -3,8 +3,15 @@
 # Change the working directory to the script's directory
 cd "$(dirname "$0")"
 
-python3 ./../convert-json-zip-to-csv.py --unzip --clear --zip_file_name "energy-variance.zip" --observable_index 0 --out_file_suffix "energy"
-python3 ./../convert-json-zip-to-csv.py                 --zip_file_name "energy-variance.zip" --observable_index 1 --out_file_suffix "variance"
+runnumbers=("run0" "run1" "run2" "run3" )
+
+python3 ./../convert-json-zip-to-csv.py --clear --zip_file_name "clear" --observable_index 0 --out_file_suffix "clear"
+
+for runnumber in "${runnumbers[@]}"; do
+    python3 ./../convert-json-zip-to-csv.py --unzip --zip_file_name "energy-variance-$runnumber.zip" --observable_index 0 --out_file_suffix "energy"
+    python3 ./../convert-json-zip-to-csv.py         --zip_file_name "energy-variance-$runnumber.zip" --observable_index 1 --out_file_suffix "variance"
+done
+
 
 gnuplot energy.gp
 gnuplot variance.gp
